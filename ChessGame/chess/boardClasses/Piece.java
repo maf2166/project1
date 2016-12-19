268473e7af66ff28ed212db41612fb93477ae17d
maf2166 <mflowers@ifes.org>
2016-12-19 17:21:31 -0500

    new peice classes

--- ChessGame/chess/boardClasses/Piece.java
+++
@@ -1,7 +0,0 @@
-package chess.boardClasses;
-
-public interface Piece {
-	public static boolean isWhite = true;
-	public static String position;
-	
-}
---
+++ ChessGame/chess/boardClasses/PieceInterface.java
@@ -0,0 +1,15 @@
+package chess.boardClasses;
+
+public interface PieceInterface {
+	 
+	
+	public char getPositionx();
+	
+	public int getPositiony();
+	
+	public void move(char positionx, int positiony);
+	
+	public void remove();
+	
+	public String getColor();
+}
